323b6753c2d70e8af91329d1d8f56c30b6921a97
omor1058 <omor1058abid@gmail.com>
2020-05-10 15:30:33 +0600

    First Last Name with console read write

---
+++ HelloWorld_Read_Write_Console/HelloWorld_Read_Write_Console/HelloWorld_Read_Write_Console/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace HelloWorld_Read_Write_Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Please enter your First Name");
+            String FirstName = Console.ReadLine();
+
+            Console.WriteLine("Please enter your Last Name");
+            String LastName = Console.ReadLine();
+
+            Console.WriteLine("Hello {0} {1}", FirstName , LastName);
+
+
+            Console.WriteLine("Welcome To The C Sharp World");
+        }
+    }
+}

19309a474f02951c23a30940abc7b5a8ceb00c4e
omor1058 <omor1058abid@gmail.com>
2020-05-11 23:28:02 +0600

    Types,Operators, Nullabletypes, Arrays

---
+++ Operators_NullableTypes_Arrays/Operators_NullableTypes_Arrays/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Operators_NullableTypes_Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int number = 10;
+            bool isNumber10 = number == 10 ? true : false;
+            Console.WriteLine("Number == 10 is {0}", isNumber10);
+
+            // Nullable example
+
+            int? TicketOnSale = 100;
+            int AvaialableTickets = TicketOnSale ?? 0;
+
+            Console.WriteLine("AvaialableTickets = {0}", AvaialableTickets);
+
+            // Datatypes Conversion
+
+            double number1 = 100.63;
+            //int number2 = (int)number1;
+            int number2 = Convert.ToInt32(number1);
+
+            //String to int
+
+            String strNumber = "100";
+            int Result = 0;
+            bool IsConversionSuccessful = int.TryParse( strNumber, out Result);
+
+            if (IsConversionSuccessful)
+            {
+                Console.WriteLine(Result);
+            }
+            else
+            {
+                Console.WriteLine("Please enter a valid number");
+            }
+
+        }
+    }
+}
---
+++ Types/Types/Types/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Types
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 25;
+            double num2 = 500.20;
+            Console.WriteLine("First Number " + num1 + " Second Number " + num2);
+            string Name = @"C:\Program Files\Internet Explorer\images";
+            Console.WriteLine(Name);
+        }
+    }
+}
